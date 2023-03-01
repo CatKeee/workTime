4cf8df68c4be45ea7d3c84b907eb1f1f8cf6b9bf
k客 <2749143951@qq.com>
2023-03-01 22:59:43 +0800

    init

---
+++ src/.vuepress/navbar/zh.ts
@@ -0,0 +1,24 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const zhNavbar = navbar([
+  { text: "指南", icon: "discover", link: "/demo/" },
+  {
+    text: "分类",
+    icon: "creative",
+    prefix: "/guide/",
+    children: [
+      {
+        text: "Bar",
+        icon: "creative",
+        prefix: "bar/",
+        children: ["baz", { text: "...", icon: "more", link: "" }],
+      },
+      {
+        text: "Foo",
+        icon: "config",
+        prefix: "foo/",
+        children: ["ray", { text: "...", icon: "more", link: "" }],
+      },
+    ],
+  }
+]);
